d2267a90e4220dd09dacc41fca3e7e1588016f9a
pcarton <pcarton@slb.com>
2016-06-15 13:19:30 -0500

    using php and includes

--- mentionslegales.php
+++ mentionslegales.php
@@ -1,160 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include("includes/header.shtml"); ?>
 
-<head>
+<?php include("includes/navigation.shtml"); ?>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Contact - Business Casual - Start Bootstrap Theme</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/business-casual.css" rel="stylesheet">
-
-    <!-- Fonts -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <div class="brand">Business Casual</div>
-    <div class="address-bar">3481 Melrose Place | Beverly Hills, CA 90210 | 123.456.7890</div>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
-                <a class="navbar-brand" href="index.html">Business Casual</a>
+<div class="container">
+    <div class="row">
+        <div class="box">
+            <div class="col-md-8 large-padding">                
+                <h2 class="intro-text">
+                    <strong>Mentions Légales</strong>
+                </h2>     
+                <table class="table">
+                  <tbody>
+                    <tr>
+                      <td>Raison sociale</td>
+                      <td>7h - 19h</td>
+                    </tr>
+                    <tr>
+                      <td>Forme juridique</td>
+                      <td>7h - 19h</td>
+                    </tr>
+                    <tr>
+                      <td>Adresse</td>
+                      <td>7h - 19h</td>
+                    </tr>
+                    <tr>                            
+                      <td>Montant du capital social</td>
+                      <td>7h - 19h</td>
+                    </tr>
+                    <tr>                            
+                      <td>Email</td>
+                      <td>contact@artdupain.fr</td>
+                    </tr>
+                    <tr>                            
+                      <td>Numéro de téléphone</td>
+                      <td>03.20.31.32.73</td>
+                    </tr>
+                    <tr>                            
+                      <td>Numéro d'inscription au registre du commerce et des sociétés (RCS)</td>
+                      <td>7h - 15h</td>
+                    </tr>
+                    <tr>                            
+                      <td>Numéro d'immatriculation au répertoire des métiers (RM)</td>
+                      <td>7h - 15h</td>
+                    </tr>
+                    <tr>                            
+                      <td>Numéro individuel d'identification fiscale </td>
+                      <td>Fermé</td>                            
+                    </tr>
+                    <tr>                            
+                      <td>Hébergeur du site</td>
+                      <td>Fermé</td>                            
+                    </tr>
+                    <tr>                            
+                      <td>Adresse</td>
+                      <td>2 rue Kellermann - 59100 Roubaix</td>                            
+                    </tr>
+                    <tr>                            
+                      <td>Téléphone</td>
+                      <td>09.72.10.10.07</td>                            
+                    </tr>
+                  </tbody>
+                </table>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="index.html">Home</a>
-                    </li>
-                    <li>
-                        <a href="about.html">About</a>
-                    </li>
-                    <li>
-                        <a href="blog.html">Blog</a>
-                    </li>
-                    <li>
-                        <a href="contact.html">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+            <div class="clearfix"></div>
         </div>
-        <!-- /.container -->
-    </nav>
-
-    <div class="container">
-
-        <div class="row">
-            <div class="box">
-                <div class="col-md-8 large-padding">
-                    <!-- Embedded Google Map using an iframe - to select your location find it on Google maps and paste the link as the iframe src. If you want to use the Google Maps API instead then have at it! -->
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1789.292911886916!2d3.0664580311262513!3d50.63747626957564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2d589ece19923%3A0xf989a9cdd1797572!2sL&#39;ART+DU+PAIN!5e0!3m2!1sfr!2sus!4v1452802372406" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen scrolling="no" marginheight="0" marginwidth="0" ></iframe>                    
-                </div>
-                <div class="col-md-4 text-center large-padding" >
-                    <h2 class="intro-text">
-                        <strong>Horaires</strong>
-                    </h2>     
-                      <table class="table">
-                        <tbody>
-                          <tr>
-                            <td>Lundi</td>
-                            <td>7h - 19h</td>
-                          </tr>
-                          <tr>
-                            <td>Mardi</td>
-                            <td>7h - 19h</td>
-                          </tr>
-                          <tr>
-                            <td>Mercredi</td>
-                            <td>7h - 19h</td>
-                          </tr>
-                          <tr>                            
-                            <td>Jeudi</td>
-                            <td>7h - 19h</td>
-                          </tr>
-                          <tr>                            
-                            <td>Vendredi</td>
-                            <td>7h - 19h</td>
-                          </tr>
-                          <tr>                            
-                            <td>Samedi</td>
-                            <td>7h - 15h</td>
-                          </tr>
-                          <tr>                            
-                            <td>Dimanche</td>
-                            <td>Ferme</td>                            
-                          </tr>
-                        </tbody>
-                      </table>
-                    <h2 class="intro-text">
-                        <strong>Coordonnees</strong>
-                    </h2>
-                    <table class="table big-text">
-                      <tbody>
-                        <tr>
-                          <td>22 rue de Roubaix, Lille
-                            <br>03.20.31.32.73
-                            <br><a href="mailto:contact@artdupain.fr">contact@artdupain.fr</a>
-                          </td>
-                        </tr>
-                        <tr>
-                        </tr>
-                      </tbody>
-                      </table>
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-        </div>
-
     </div>
-    <!-- /.container -->
+</div>
 
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+<?php include("includes/footer.shtml"); ?>
---
+++ pratique.php
@@ -0,0 +1,71 @@
+<?php include("includes/header.shtml"); ?>
+
+<?php include("includes/navigation.shtml"); ?>
+
+<div class="container">
+
+    <div class="row">
+        <div class="box">
+            <div class="col-md-8 large-padding">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1789.292911886916!2d3.0664580311262513!3d50.63747626957564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2d589ece19923%3A0xf989a9cdd1797572!2sL&#39;ART+DU+PAIN!5e0!3m2!1sfr!2sus!4v1452802372406" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen scrolling="no" marginheight="0" marginwidth="0" ></iframe>                    
+            </div>
+            <div class="col-md-4 text-center large-padding" >
+                <h2 class="intro-text">
+                    <strong>Horaires</strong>
+                </h2>     
+                  <table class="table">
+                    <tbody>
+                      <tr>
+                        <td>Lundi</td>
+                        <td>7h - 19h</td>
+                      </tr>
+                      <tr>
+                        <td>Mardi</td>
+                        <td>7h - 19h</td>
+                      </tr>
+                      <tr>
+                        <td>Mercredi</td>
+                        <td>7h - 19h</td>
+                      </tr>
+                      <tr>                            
+                        <td>Jeudi</td>
+                        <td>7h - 19h</td>
+                      </tr>
+                      <tr>                            
+                        <td>Vendredi</td>
+                        <td>7h - 19h</td>
+                      </tr>
+                      <tr>                            
+                        <td>Samedi</td>
+                        <td>7h - 15h</td>
+                      </tr>
+                      <tr>                            
+                        <td>Dimanche</td>
+                        <td>Fermé</td>                            
+                      </tr>
+                    </tbody>
+                  </table>
+                <h2 class="intro-text">
+                    <strong>Coordonnées</strong>
+                </h2>
+                <table class="table big-text">
+                  <tbody>
+                    <tr>
+                      <td>22, rue de Roubaix, Lille
+                        <br>03.20.31.32.73
+                        <br><a href="mailto:contact@artdupain.fr">contact@artdupain.fr</a>
+                      </td>
+                    </tr>
+                    <tr>
+                    </tr>
+                  </tbody>
+                  </table>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+
+</div>
+
+<?php include("includes/footer.shtml"); ?>
